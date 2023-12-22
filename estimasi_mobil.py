a92f2f40bf2a84732d510a29323c9ec427f78313
Namakudana <117477135+Namakudana@users.noreply.github.com>
2023-12-22 10:19:43 +0700

    new

--- estimasi_mobil.py
+++ estimasi_mobil.py
@@ -3,15 +3,16 @@ import streamlit as st
 
 model = pickle.load(open('estimasi_mobil_toyota_webOTO.sav', 'rb'))
 
+# title
 st.title('Estimasi Harga Mobil Bekas')
 
+# input
 year = st.number_input('Input Tahun Mobil')
 mileage = st.number_input('Input Km Mobil')
 tax = st.number_input('Input Pajak Mobil')
 cc = st.number_input('Input CC mesin')
-#seat = st.number_input('muatan kursi mobil')
-# mpg = st.number_input('Kebutuhan rata-rata bahan bakar dalam satuan mpg')
 
+# predict
 predict = ''
 
 if st.button('Estimasi Harga'):
@@ -21,4 +22,3 @@ if st.button('Estimasi Harga'):
   
 st.write ('Estimasi Harga Mobil Bekas Dalam Rupiah IDR (Juta) : ', predict)
 st.write ('NOTE : angka dibelakang (.) tidak dianggap')
-#st.write ('Estimasi Harga Mobil Bekas Dalam Rupiah (IDR) : ', predict*19625)
